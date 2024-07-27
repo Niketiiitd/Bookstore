1e890e1d9f208e39fe640c5a9c58c7374087f34b
Niketiiitd <niket22320@iiitd.ac.in>
2024-07-27 19:13:18 +0530

    dot env configed

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 // module js code 
-
-
-
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 7777;
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
