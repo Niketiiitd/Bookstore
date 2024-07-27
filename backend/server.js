1e8e635c1b61d3ec8e09ca9ae84d1cad293031d3
Niketiiitd <niket22320@iiitd.ac.in>
2024-07-27 18:55:34 +0530

    backend set up done

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+// module js code 
+
+
+
+
+const app = express();
+const port = process.env.PORT || 7777;
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
