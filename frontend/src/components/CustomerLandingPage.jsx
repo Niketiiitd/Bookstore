e34fdc90a261f4f2986738bfb781b45aca9ee892
Niketiiitd <niket22320@iiitd.ac.in>
2024-08-04 22:24:40 +0530

    progress on customer landing page

--- frontend/src/components/CustomerLandingPage.jsx
+++ frontend/src/components/CustomerLandingPage.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-
+import Navbar from './Navbar';
 function CustomerLandingPage({ customerId }) {
     return (
-        <div className="flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold">Welcome Customer {customerId}</h1>
+
+        <>
+        
+        
+        <div className="flex flex-col">
+            <div className="mt-11 mx-11 h-10 bg-gray-100">
+                <Navbar />
+
+            </div>
+            
             <p className="text-lg mt-4">This is your landing page</p>
         </div>
+        </>
     );
 }
 export default CustomerLandingPage;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbSeparator,
+  } from '@chakra-ui/react';
+import { ChevronRightIcon } from '@chakra-ui/icons';
+function Navbar() {
+    return(
+        <>
+        <Breadcrumb spacing='8px' separator={<ChevronRightIcon color='gray.500' />}>
+            <BreadcrumbItem>
+                <BreadcrumbLink href='#'>Home</BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem>
+                <BreadcrumbLink href='#'>About</BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem isCurrentPage>
+                <BreadcrumbLink href='#'>Contact</BreadcrumbLink>
+            </BreadcrumbItem>
+            </Breadcrumb>
+        </>
+    );
+
+}
+export default Navbar;
