c493adb6b581b2152ea7d487346f8d57d9832814
Niketiiitd <niket22320@iiitd.ac.in>
2024-08-07 06:28:48 +0530

    Update server.js

--- frontend/src/components/CustomerLandingPage.jsx
+++ frontend/src/components/CustomerLandingPage.jsx
@@ -2,9 +2,9 @@ import { ChevronRightIcon } from '@chakra-ui/icons';
 import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react';
 import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Cart from './customer/Cart';
 import Home from './customer/Home';
 import Profile from './customer/Profile';
-
 function CustomerLandingPage({ customerId }) {
   return (
     <>
@@ -12,7 +12,7 @@ function CustomerLandingPage({ customerId }) {
         <div className="flex flex-col">
           <div className="mt-11 mx-11 h-10 bg-gray-100">
             <Box display="flex" alignItems="center">
-              <Breadcrumb spacing='8px' separator={<ChevronRightIcon color='gray.500' />}>
+              <Breadcrumb spacing='25px' separator={<ChevronRightIcon color='gray.500' />}>
                 <BreadcrumbItem>
                   <BreadcrumbLink as={Link} to='/'>Home</BreadcrumbLink>
                 </BreadcrumbItem>
@@ -24,15 +24,22 @@ function CustomerLandingPage({ customerId }) {
                 </BreadcrumbItem>
               </Breadcrumb>
               <Box marginLeft="auto">
+                <div className='flex mx-3'>
                 <Link to='/Profile'>
                   <button>Profile</button>
                 </Link>
+                <Link to='/Cart' className='ml-4'>
+                  <button>Cart</button>
+                </Link>
+                </div>
               </Box>
             </Box>
           </div>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/Profile" element={<Profile />} />
+            <Route path="/Profile" element={<Profile customerId={customerId}/>} />
+            <Route path="/Cart" element={<Cart customerId={customerId}/>} />
+
           </Routes>
         </div>
       </BrowserRouter>
