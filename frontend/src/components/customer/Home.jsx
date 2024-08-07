00fdbb46b19a5c076fe7b0d0b00efe3427ff5b03
Niketiiitd <niket22320@iiitd.ac.in>
2024-08-07 06:28:54 +0530

    Update Home.jsx

--- frontend/src/components/customer/Home.jsx
+++ frontend/src/components/customer/Home.jsx
@@ -1,6 +1,44 @@
-import React, { useState, useEffect } from "react";
+import { Box, Image, SimpleGrid, Text } from '@chakra-ui/react'; // Assuming you are using Chakra UI
 import axios from 'axios';
-import { SimpleGrid, Box, Image, Text } from '@chakra-ui/react'; // Assuming you are using Chakra UI
+import React, { useEffect, useState } from "react";
+
+import useHover from './utils/useHover';
+function BookItem({ book }) {
+  const [hoverRef, isHovered] = useHover(); return (
+    <Box
+      key={book.id}
+      borderWidth='1px'
+      borderRadius='lg'
+      overflow='hidden'
+      mb='4'
+      ref={hoverRef}
+      bg={isHovered ? 'gray.100' : 'white'}
+      transition='background-color 0.3s'
+    >
+      <Image src={book.imageUrl} alt='Book_Image' />
+      <Box p='6'>
+        <Box d='flex' alignItems='baseline'>
+          <Text fontWeight='bold' as='h4' lineHeight='tight' isTruncated>
+            {book.book_title}
+          </Text>
+        </Box>
+        <Box>
+          {'Rs ' + book.book_price + '/-'}
+        </Box>
+        <Box>
+          <Text as='h4' lineHeight='tight' isTruncated>
+            {book.book_genre + ' | ' + book.book_author + ' | ' + book.book_publication}
+          </Text>
+        </Box>
+        <Box>
+          <Text as='h3' lineHeight='tight' isTruncated>
+            {'In stock -> '+ book.book_availability}
+          </Text>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
 
 function Home() {
   const [books, setBooks] = useState([]);
@@ -18,11 +56,6 @@ function Home() {
       });
   };
 
-  const handleProfileClick = () => {
-    console.log('Profile clicked');
-    setShowProfile(true);
-  };
-
   useEffect(() => {
     allBookFetch();
   }, []);
@@ -32,24 +65,7 @@ function Home() {
       {books.length > 0 ? (
         <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={10}>
           {books.map((book) => (
-            <Box key={book.id} borderWidth='1px' borderRadius='lg' overflow='hidden' mb='4'>
-              <Image src={book.imageUrl} alt='Book_Image' />
-              <Box p='6'>
-                <Box d='flex' alignItems='baseline'>
-                  <Text fontWeight='bold' as='h4' lineHeight='tight' isTruncated>
-                    {book.book_title}
-                  </Text>
-                </Box>
-                <Box>
-                  {'Rs ' + book.book_price + '/-'}
-                </Box>
-                <Box>
-                  <Text as='h4' lineHeight='tight' isTruncated>
-                    {book.book_genre + ' | ' + book.book_author}
-                  </Text>
-                </Box>
-              </Box>
-            </Box>
+            <BookItem key={book.id} book={book} />
           ))}
         </SimpleGrid>
       ) : (
